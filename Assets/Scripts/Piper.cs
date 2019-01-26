98d5e29056edbbf05dfc5f64195c4734cc2e6504
DiegoArmando <thespian42@gmail.com>
2019-01-26 15:27:36 -0500

    Now you can pick up sticks

--- Assets/Scripts/Piper.cs
+++ Assets/Scripts/Piper.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Piper : MonoBehaviour
 {
 
-    List<GameObject> sticks;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Piper loaded");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Stick")
+        {
+            Debug.Log("Yep, that's a stick");
+            collision.gameObject.SendMessage("Become_Collectable");
+        }
+
+        //if (collision.relativeVelocity.magnitude > 2)
+        //    audioSource.Play();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Stick")
+        {
+            collision.gameObject.SendMessage("Become_Uncollectable");
+        }
+
+        //if (collision.relativeVelocity.magnitude > 2)
+        //    audioSource.Play();
     }
 }
